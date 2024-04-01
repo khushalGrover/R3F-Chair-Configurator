a8fc8fc2bf860123b49d9f78c40e5f7d10c3621f
Khushal Grover <khushal15grover@gmail.com>
2024-04-01 06:24:34 +0530

    front end completed

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,138 +3,78 @@ import "./menu.css"; // Import menu.css file
 import Button from "./Button"; // Import your custom Button component
 
 export function Menu() {
-	
-	return (
-		<nav id="colorNav">
-			<ul>
-				<li className="red">
-					
-					<img src="./Icon/chair-arm.png" alt="chairBack-image" />{" "}
-					
-					<DropDownList />
-				</li>
-				<li className="purple">
-				
-						<img
-							src="./Icon/chair-back.png"
-							alt="chairBack-image"
-						/>
-					
-					<ul>
-						<li>
-							<a href="#">None</a>
-						</li>
-						<li>
-							<a href="#">Classic Back</a>
-						</li>
-						<li>
-							<a href="#">Moderen Back</a>
-						</li>
-						<li>
-							<a href="#">Royal Back</a>
-						</li>
-					</ul>
-				</li>
-				<li className="green">
-					
-						<img
-							src="./Icon/chair-cushion.png"
-							alt="chairSeat-image"
-						/>
-					
-					<ul>
-						<li>
-							<a href="#">None</a>
-						</li>
-						<li>
-							<a href="#">Classic Cushion</a>
-						</li>
-						<li>
-							<a href="#">Moderen Cushion</a>
-						</li>
-						<li>
-							<a href="#">Royal Cushion</a>
-						</li>
-					</ul>
-				</li>
-				<li className="blue">
-					
-						<img
-							src="./Icon/chair-legs.png"
-							alt="chairLegs-image"
-						/>
-					
-					<ul>
-						<li>
-							<a href="#">Classic Legs</a>
-						</li>
-						<li>
-							<a href="#">Moderen Legs</a>
-						</li>
-						<li>
-							<a href="#">Royal Legs</a>
-						</li>
-					</ul>
-				</li>
-				<li className="orange">
-					
-						<img
-							src="./Icon/chair-cushion.png"
-							alt="chair sets image"
-						/>
-					
-					<ul>
-						<li>
-							<a href="#">Classic Seat</a>
-						</li>
-						<li>
-							<a href="#">Modere Seat</a>
-						</li>
-						<li>
-							<a href="#">Royal Seat</a>
-						</li>
-					</ul>
-				</li>
-				<li className="yellow">
-					
-						<img src="./Icon/chair.png" alt="chair sets image" />
-					
-					<ul>
-						<li>
-							{/* <a href="#">Classic Set</a>  */}
-							<Button size="lg" variant="dropdown" onClick={console.log("yeah pressed")}>
-								Classic Set
-							</Button>
-						</li>
-						<li>
-							<a href="#">Modere Set</a>
-						</li>
-						<li>
-							<a href="#">Royal Set</a>
-						</li>
-					</ul>
-				</li>
-			</ul>
-		</nav>
-	);
-
-
-	function DropDownList() {
+	const MenuItem = ({ color, icon, label, children }) => {
 		return (
-			<ul>
-				<li>
-					<a href="#">None</a>
-				</li>
-				<li>
-					<a href="#">Classic Arm</a>
-				</li>
-				<li>
-					<a href="#">Moderen Arm</a>
-				</li>
-				<li>
-					<a href="#">Royal Arm</a>
-				</li>
+			<li className={`${color} relative`}>
+				<img src={icon} alt="icon" />
+				{label && <DropDownList>{label}</DropDownList>}
+				{children}
+			</li>
+		);
+	};
+
+	function DropDownList({ children }) {
+		if (!Array.isArray(children)) {
+			children = [children];
+		}
+
+		return (
+			<ul className="h-[250px]">
+				{children.map((item, index) => (
+					<li key={index}>
+						<Button variant="dropdown">
+							{item}
+						</Button>
+					</li>
+				))}
 			</ul>
 		);
 	}
+
+	return (
+		<nav id="colorNav" className="flex justify-center mt-10">
+			<ul className="flex space-x-4">
+				<MenuItem
+					color="red"
+					icon="./Icon/chair-arm.png"
+					label={["None", "Classic Arm", "Modern Arm", "Royal Arm"]}
+				/>
+				<MenuItem
+					color="blue"
+					icon="./Icon/chair-back.png"
+					label={[
+						"None",
+						"Classic Back",
+						"Modern Back",
+						"Royal Back",
+					]}
+				/>
+				<MenuItem
+					color="purple"
+					icon="./Icon/chair-cushion.png"
+					label={[
+						"None",
+						"Classic cushion",
+						"Modern cushion",
+						"Royal cushion",
+					]}
+				/>
+				<MenuItem
+					color="green"
+					icon="./Icon/chair-legs.png"
+					label={["Classic Legs", "Modern Legs", "Royal Legs"]}
+				/>
+				<MenuItem
+					color="orange"
+					icon="./Icon/chair-cushion.png"
+					label={["Classic seat", "Modern seat", "Royal seat"]}
+				/>
+				<MenuItem
+					color="yellow"
+					icon="./Icon/chair.png"
+					label={["Classic set", "Modern set", "Royal set"]}
+				/>
+			</ul>
+		</nav>
+	);
 }
