2152e03950f2208260a14cb904b5a509c3710900
khushalGrover <khushal15grover@gmail.com>
2024-05-03 23:38:53 +0530

    pointer cursor working

--- src/components/Chairs_2.jsx
+++ src/components/Chairs_2.jsx
@@ -1,57 +1,63 @@
 import React, { useEffect, useState } from "react";
-import { useGLTF } from "@react-three/drei";
+import { useGLTF, useCursor } from "@react-three/drei";
 import { useCustomization } from "../Constants/Customization";
 
 export function Chairs_2(props) {
-    const { nodes, materials } = useGLTF("./Models/v2/chairs_2.gltf");
-    const { partToMeshMapping } = useCustomization();
-    const [hoverObject, setHoverObject] = useState("");
+	const { nodes, materials } = useGLTF("./Models/v2/chairs_2.gltf");
+	const { partToMeshMapping } = useCustomization();
+	const [hoverObject, setHoverObject] = useState("");
+	const [hover, setHover] = useState(false);
 
-    useEffect(() => {
-        console.log(hoverObject);
-    }, [hoverObject]);
+    useCursor(hover);
 
-    const handlePointerOver = (part) => (event) => {
-        event.stopPropagation();
-        setHoverObject(part);
-    };
+	useEffect(() => {
+		console.log(hoverObject);
+		hoverObject !== "" ? setHover(true) : setHover(false);
+	}, [hoverObject]);
 
-    const handlePointerOut = () => {
-        setHoverObject("");
-    };
+	useEffect(() => {}, [hoverObject]);
 
-    const renderMesh = (part, geometry) => {
-        if (partToMeshMapping[part] === 0) return null;
+	const handlePointerOver = (part) => (event) => {
+		event.stopPropagation();
+		setHoverObject(part);
+	};
 
-        return (
-            <mesh
-                key={part}
-                geometry={geometry}
-                material={materials.chair}
-                visible={partToMeshMapping[part] === 1}
-                onPointerOver={handlePointerOver(part)}
-                onPointerOut={handlePointerOut}
-            >
-                <meshStandardMaterial
-                    color={hoverObject === part ? "hotpink" : "orange"}
-                />
-            </mesh>
-        );
-    };
+	const handlePointerOut = () => {
+		setHoverObject("");
+	};
 
-    const chairParts = [
-        { part: "A", geometry: nodes.classic_Arm.geometry },
-        { part: "B", geometry: nodes.classic_Back.geometry },
-        { part: "C", geometry: nodes.classic_Cushion.geometry },
-        { part: "S", geometry: nodes.classic_Seat.geometry },
-        { part: "L", geometry: nodes.classic_Legs.geometry },
-    ];
+	const renderMesh = (part, geometry) => {
+		if (partToMeshMapping[part] === 0) return null;
 
-    return (
-        <group {...props} dispose={null} scale={[0.1, 0.1, 0.1]}>
-            {chairParts.map(({ part, geometry }) => renderMesh(part, geometry))}
-        </group>
-    );
+		return (
+			<mesh
+				key={part}
+				geometry={geometry}
+				material={materials.chair}
+				visible={partToMeshMapping[part] === 1}
+				onPointerOver={handlePointerOver(part)}
+				onPointerOut={handlePointerOut}
+			>
+				<meshStandardMaterial
+					color={hoverObject === part ? "hotpink" : "orange"}
+				/>
+			</mesh>
+		);
+	};
+
+	const chairParts = [
+		{ part: "A", geometry: nodes.classic_Arm.geometry },
+		{ part: "B", geometry: nodes.classic_Back.geometry },
+		{ part: "C", geometry: nodes.classic_Cushion.geometry },
+		{ part: "S", geometry: nodes.classic_Seat.geometry },
+		{ part: "L", geometry: nodes.classic_Legs.geometry },
+	];
+
+	return (
+		<group {...props} dispose={null} scale={[0.1, 0.1, 0.1]}>
+			{chairParts.map(({ part, geometry }) => renderMesh(part, geometry))}
+		</group>
+	);
 }
 
 useGLTF.preload("./Models/v2/chairs_2.gltf");
